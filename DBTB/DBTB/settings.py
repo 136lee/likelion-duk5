b5fc37ad917fcbf3c2b27a0008e09c4d38fc04f2
leeyumin626 <leeym7404@naver.com>
2025-08-11 19:17:30 +0900

    모델 생성

--- DBTB/DBTB/settings.py
+++ DBTB/DBTB/settings.py
@@ -119,12 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS=[
     BASE_DIR / 'static',
@@ -132,3 +133,5 @@ STATICFILES_DIRS=[
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = 'user.User'
--- DBTB/post/models.py
+++ DBTB/post/models.py
@@ -1,12 +1,34 @@
 from django.db import models
-from django.contrib.auth import get_user_model
-from map.models import Place
+from user.models import User
+import os
+from uuid import uuid4
+from django.utils import timezone
 
-User = get_user_model()
+def upload_filepath(instance, filename):
+    today_str = timezone.now().strftime("Y%m%d")
+    file_basename = os.path.basename(filename)
+    return f'{instance._meta.model_name}/{today_str}/{str(uuid4())}_{file_basename}'
 
+class Category(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.name
+    
 class Post(models.Model):
-    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name="posts")
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    rating = models.IntegerField()
+    title = models.CharField(max_length=50)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="posts")
+    category = models.ManyToManyField(to=Category, through="PostCategory", related_name="posts")
+    image = models.ImageField(upload_to=upload_filepath, blank=True)
+    video = models.FileField(upload_to=upload_filepath, blank=True)
+
+
+
+    def __str__(self):
+        return f'[{self.id}] {self.title}'
+    
+class PostCategory(models.Model):
+    post = models.ForeignKey(to=Post, on_delete=models.CASCADE, related_name="post_categories")
+    category = models.ForeignKey(to=Category, on_delete=models.CASCADE, related_name="post_categories")
