1dd20a399fcc612edcfd808e12698d573aeaff22
aney0714 <gimhh770@gmail.com>
2025-08-20 03:42:27 +0900

    메인, 피드, 탐험 페이지

---
+++ DBTB/static/js/account/feed.js
@@ -0,0 +1,10 @@
+document.getElementById("move_to_map").addEventListener("click", function() {
+    window.location.href = "/DBTB/templates/main/main.html";
+  });
+  document.getElementById("move_to_feed").addEventListener("click", function() {
+    window.location.href = "/DBTB/templates/feed/feed.html";
+  });
+  document.getElementById("move_to_EX").addEventListener("click", function() {
+    window.location.href = "/DBTB/templates/explore/explore.html";;
+  });
+  
